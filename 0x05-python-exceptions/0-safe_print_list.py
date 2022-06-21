1529b91b7785dd3df1b6c9f556b425bb7b157dc8
mathwa <matechnolj19@gmail.com>
2022-06-20 17:15:31 -0700

    safe print list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    count = 0
+def safe_print_integer(value)
 
-    for i in range(x):
-        try:
-            print("{}".format(my_list[i]), end='')
-        except:
-            break
-        else:
-            count += 1
-
-    print()
-    return (count)
+    try:
+       print("{:d}".format(value))
+       return True
+    except(ValueError, TypeError):
+       return False
+    return True
